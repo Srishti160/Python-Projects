7184d7a3f2801f31b318d98d6964bde22de3b5ee
Srishti Kumari <learn.coding.with.we@gmail.com>
2025-05-05 00:41:17 +0530

    adding features in guess the num

--- python/guess_num.py
+++ python/guess_num.py
@@ -1,18 +1,38 @@
 import random
-num= random.randint(1,100)
-while True:
+start=int(input("Enter the starting range of number :"))
+stop= int(input("Enter ending range of the number :"))
+limit=int(input("Enter the limit of guesses a player can make :"))
+try:
+    num= random.randint(start,stop)
+except ValueError:
+    print("Please enter valid range")
+
+flag=0
+n=1
+count=0
+
+while n<=limit:
     try:
-        guess=int(input("Guess the number between 1-100 :"))
+        count=count+1
+        guess=int(input("Guess the number :"))
         if guess>num:
-            print("Too High")
-            continue
+            print("Too High!")
         elif guess<num:
-            print("Too low")
-            continue
+            print("Too low!")
         elif guess==num:
             print("Congrats! You have guess correct number..")
+            flag=1
             break
         else:
             print("Invalid Input!") 
+
     except ValueError:
-        print("Please enter valid number (between 1 to 100)")
+        print("Please enter valid number ")
+    
+    n=n+1
+
+if flag==0:
+    print("You ran out of moves!")
+    print("The number was", num)
+else:
+    print("Best Score:",count)
